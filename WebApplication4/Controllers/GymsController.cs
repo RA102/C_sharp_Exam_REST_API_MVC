664865836c1db74dc2cda6d26caa060ad25e9fa6
RA <ra_lebedev@mail.ru>
2020-02-06 15:25:21 +0600

    !

---
+++ WebApplication4/Controllers/GymsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class GymsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Gyms
+        public IQueryable<Gym> GetGyms()
+        {
+            return db.Gyms;
+        }
+
+        // GET: api/Gyms/5
+        [ResponseType(typeof(Gym))]
+        public IHttpActionResult GetGym(int id)
+        {
+            Gym gym = db.Gyms.Find(id);
+            if (gym == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(gym);
+        }
+
+        // PUT: api/Gyms/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutGym(int id, Gym gym)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != gym.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(gym).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GymExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Gyms
+        [ResponseType(typeof(Gym))]
+        public IHttpActionResult PostGym(Gym gym)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Gyms.Add(gym);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = gym.Id }, gym);
+        }
+
+        // DELETE: api/Gyms/5
+        [ResponseType(typeof(Gym))]
+        public IHttpActionResult DeleteGym(int id)
+        {
+            Gym gym = db.Gyms.Find(id);
+            if (gym == null)
+            {
+                return NotFound();
+            }
+
+            db.Gyms.Remove(gym);
+            db.SaveChanges();
+
+            return Ok(gym);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GymExists(int id)
+        {
+            return db.Gyms.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApplication4/Controllers/TimingTrainingsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class TimingTrainingsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/TimingTrainings
+        public IQueryable<TimingTraining> GetTimingTrainings()
+        {
+            var timingTrainings = db.TimingTrainings.Include(g => g.Gym).Include(c => c.Coach).Include(t => t.Type);
+            return timingTrainings;
+        }
+
+        // GET: api/TimingTrainings/5
+        [ResponseType(typeof(TimingTraining))]
+        public IHttpActionResult GetTimingTraining(int id)
+        {
+            TimingTraining timingTraining = db.TimingTrainings.Find(id);
+            if (timingTraining == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(timingTraining);
+        }
+
+        // PUT: api/TimingTrainings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTimingTraining(int id, TimingTraining timingTraining)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != timingTraining.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(timingTraining).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TimingTrainingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TimingTrainings
+        [ResponseType(typeof(TimingTraining))]
+        public IHttpActionResult PostTimingTraining(TimingTraining timingTraining)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TimingTrainings.Add(timingTraining);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = timingTraining.Id }, timingTraining);
+        }
+
+        // DELETE: api/TimingTrainings/5
+        [ResponseType(typeof(TimingTraining))]
+        public IHttpActionResult DeleteTimingTraining(int id)
+        {
+            TimingTraining timingTraining = db.TimingTrainings.Find(id);
+            if (timingTraining == null)
+            {
+                return NotFound();
+            }
+
+            db.TimingTrainings.Remove(timingTraining);
+            db.SaveChanges();
+
+            return Ok(timingTraining);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TimingTrainingExists(int id)
+        {
+            return db.TimingTrainings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
